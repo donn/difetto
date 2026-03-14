bf91f85f4a5f6e3133524420d7ce7f5f87401eea
Mohamed Gaber <me@donn.website>
2026-03-14 05:55:15 +0200

    add picorv32, update documentation

--- Readme.md
+++ Readme.md
@@ -54,13 +54,15 @@ example.
 
 Here are the commands for the included example:
 
+> Note: The order of two the two states passed to DifettoTest matters.
+
 ```bash
-python3 -m librelane ./test/spm/config.yaml --run-tag new_pnr --flow DifettoPNR --overwrite
+python3 -m librelane ./test/spm/config.yaml --run-tag pnr --flow DifettoPNR --overwrite
 python3 -m librelane ./test/spm/config.yaml --run-tag atpg --flow DifettoATPG --overwrite \
-  --with-initial-state ./test/spm/runs/new_pnr/*-difetto-cut/state_out.json
+  --with-initial-state ./test/spm/runs/pnr/*-difetto-cut/state_out.json
 python3 -m librelane ./test/spm/config.yaml --run-tag test --flow DifettoTest --overwrite \
   --with-initial-state ./test/spm/runs/atpg/*-difetto-quaighsim/state_out.json \
-  --with-initial-state ./test/spm/runs/new_pnr/*-difetto-chain/state_out.json
+  --with-initial-state ./test/spm/runs/pnr/*-difetto-chain/state_out.json
 ```
 
 # Current Limitations
--- test/test_difetto.py
+++ test/test_difetto.py
@@ -5,11 +5,9 @@ from decimal import Decimal
 from librelane.flows import Flow
 
 
-@pytest.mark.parametrize("design", (("spm", 100),))
-def test_difetto(design):
+def run_difetto(design_name, expected_coverage):
     os.environ["YOSYS_PLUGIN_PATH"] = str(pytest.test_root.parent / "yosys-plugin")
 
-    design_name, expected_coverage = design
     ATPG = Flow.factory.get("DifettoATPG")
     PNR = Flow.factory.get("DifettoPNR")
     Test = Flow.factory.get("DifettoTest")
@@ -17,18 +15,23 @@ def test_difetto(design):
     cfg = str(pytest.test_root / design_name / "config.yaml")
 
     pnr = PNR(cfg, pdk="sky130A")
-    pnr_out = pnr.start(run_tag="pnr_test", overwrite=True)
+    pnr_out = pnr.start(tag="pnr_test", overwrite=True)
 
     atpg = ATPG(cfg, pdk="sky130A")
-    atpg_out = atpg.start(
-        run_tag="atpg_test", overwrite=True, with_initial_state=pnr_out
-    )
+    atpg_out = atpg.start(tag="atpg_test", overwrite=True, with_initial_state=pnr_out)
 
     test = Test(cfg, pdk="sky130A")
-    test_out = test.start(
-        run_tag="test_test", overwrite=True, with_initial_state=atpg_out
-    )
+    test_out = test.start(tag="test_test", overwrite=True, with_initial_state=atpg_out)
 
     assert test_out.metrics["design__atpg__coverage"] >= Decimal(
         expected_coverage
     ), "Failed to meet expected coverage"
+
+
+def test_spm():
+    run_difetto("spm", 99.5)
+
+
+@pytest.mark.large
+def test_picorv32():
+    run_difetto("picorv32", 98)
